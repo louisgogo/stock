a3fd85c6cf5c579fe5e20e736e9391310246a0aa
louisgogo <louse12345@163.com>
2017-03-02 19:25:26 +0800

    小修改

--- stock/dateBase.py
+++ stock/dateBase.py
@@ -12,12 +12,18 @@ def dataBase_Create():
                            user='root', passwd='888888', db='mysql', charset='utf8')
     cur = conn.cursor()
     try:
-        cur.execute("DROP DATABASE stock")
         cur.execute('CREATE DATABASE stock')
     except Exception as e:
-        cur.execute('CREATE DATABASE stock')
+        print("数据库已经存在")
     cur.execute('USE job_CD')
     # 建立数据库表格
+    # 设计数据库的表头：
+    # 上市公司基本信息包括：上市公司编号、上市公司简称、行业类别
+    # 上市公司财务数据：
+    # 资产负债表—上市公司编号、上市公司简称、报告期、报告类型、科目名称、金额
+    # 利润表—
+    # 现金流量表—
+    # 公司综合能力指标—
     try:
         cur.execute('CREATE TABLE work (row_Id BIGINT(10) NOT NULL AUTO_INCREMENT,job_Id VARCHAR(200) NOT NULL,job_Name VARCHAR(200) ,job_Link VARCHAR(600),job_Wage VARCHAR(300),job_AverWage VARCHAR(200),company_Id VARCHAR(200),company_Name VARCHAR(200),company_Link VARCHAR(600),company_Nature VARCHAR(200),company_Scale VARCHAR(200),company_Area VARCHAR(400),company_Address VARCHAR(500),job_PeopleNum VARCHAR(400),job_Issue date,job_Article TEXT(20000),created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (row_Id,job_Id))')
         cur.execute("CREATE TABLE workindex (row_Id BIGINT(10) NOT NULL AUTO_INCREMENT,job_Id VARCHAR(200) NOT NULL,job_Link VARCHAR(600),created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (row_Id,job_Id))")
@@ -27,6 +33,7 @@ def dataBase_Create():
     cur.close()
     conn.close()
 
+
 def dataBase_Drop():
     conn = pymysql.connect(host='127.0.0.1', port=3306,
                            user='root', passwd='888888', db='mysql', charset='utf8')
@@ -34,6 +41,6 @@ def dataBase_Drop():
     try:
         cur.execute("DROP DATABASE stock")
     except Exception as e:
-        print("数据库不能存在")
+        print("数据库不存在")
     cur.close()
     conn.close()
