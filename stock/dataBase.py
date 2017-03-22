976058bbcbfb419348921431ecf647eb2db2b58d
louisgogo <louse12345@163.com>
2017-03-23 00:45:55 +0800

    继续完善

---
+++ stock/dataBase.py
@@ -0,0 +1,41 @@
+import pymysql
+
+
+def connection():
+    conn = pymysql.connect(host='127.0.0.1', port=3306,
+                           user='root', passwd='888888', db='mysql', charset='utf8')
+    cur = conn.cursor()
+    try:
+        cur.execute("USE stock")
+    except Exception as e:
+        pass
+    return conn
+
+
+def build():
+    conn = connection()
+    cur = conn.cursor()
+    try:
+        cur.execute("DROP DATABASE stock")
+        cur.execute('CREATE DATABASE stock')
+        print("删除旧数据库并建立新数据库")
+    except Exception as e:
+        cur.execute('CREATE DATABASE stock')
+        print("新建立数据库")
+    # 建立数据库表格
+    cur.execute("USE stock")
+    # 建立数据表格：公司列表（股票代码，公司名称，行业类别），资产负债表（股票代码，科目名称，金额<借方正数，贷方负数>，单位，所属年份，所属报告期），利润表（股#票代码，科目名称，金额<贷方正数，借方负数>，单位，所属年份，所属报告期），现金流量表（股票代码，项目名称，金额<借方正数，贷方负数>，单位，所属年份，所属报告期），公司综合能力指标（股票代码，项目名称，数值，单位，所属年份，所属报告期）
+    try:
+        cur.execute('CREATE TABLE company (row_Id BIGINT(10) NOT NULL AUTO_INCREMENT,company_Id VARCHAR(200) NOT NULL,company_Name VARCHAR(200),company_Place VARCHAR(100),company_Industry VARCHAR(600),created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (row_Id,company_Id))')
+        cur.execute("CREATE TABLE balancesheet (row_Id BIGINT(10) NOT NULL AUTO_INCREMENT,company_Id VARCHAR(200) NOT NULL,accounting VARCHAR(600),amount DECIMAL(15,2),unit VARCHAR(10),year VARCHAR(20),period VARCHAR(20),created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (row_Id,company_Id))")
+        cur.execute("CREATE TABLE incomestatements (row_Id BIGINT(10) NOT NULL AUTO_INCREMENT,company_Id VARCHAR(200) NOT NULL,accounting VARCHAR(600),amount DECIMAL(15,2),unit VARCHAR(10),year VARCHAR(20),period VARCHAR(20),created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (row_Id,company_Id))")
+        cur.execute("CREATE TABLE cashflow (row_Id BIGINT(10) NOT NULL AUTO_INCREMENT,company_Id VARCHAR(200) NOT NULL,accounting VARCHAR(600),amount DECIMAL(15,2),unit VARCHAR(10),year VARCHAR(20),period VARCHAR(20),created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (row_Id,company_Id))")
+        cur.execute("CREATE TABLE financialreport (row_Id BIGINT(10) NOT NULL AUTO_INCREMENT,company_Id VARCHAR(200) NOT NULL,accounting VARCHAR(600),amount DECIMAL(5,4),unit VARCHAR(10),year VARCHAR(20),period VARCHAR(20),created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (row_Id,company_Id))")
+        print("数据库建立完毕")
+    except (AttributeError):
+        print('TABLE已经存在')
+    cur.close()
+    conn.close()
+
+if __name__ == '__main__':
+    build()
--- stock/dataCollection.py
+++ stock/dataCollection.py
@@ -16,25 +16,84 @@ def company_Collection():
         'Accept': 'text/css,*/*;q=0.1',
         'Connection': 'keep-alive'
     }
-    while True:
+    condition = True
+    while condition:
         try:
             html = requests.get(url, headers=headers)
             html.encoding = "utf-8"
             BsObj = BeautifulSoup(html.text, 'html.parser')
-            companyList = BsObj.findAll("ul", {'class': 'company-list'})
-            print(html.status_code)
+            companyList_SZ = BsObj.find(
+                "div", {'id': 'con-a-1'}).findAll("li")
+            companyList_ZX = BsObj.find(
+                "div", {'id': 'con-a-2'}).findAll("li")
+            companyList_CY = BsObj.find(
+                "div", {'id': 'con-a-3'}).findAll("li")
+            companyList_SH = BsObj.find(
+                "div", {'id': 'con-a-4'}).findAll("li")
+            if html.status_code == 200:
+                condition = False
         except Exception as e:
             print("网页读取有问题：", e)
-        else:
-            break
-    for i in companyList:
-        company_Id = re.findall(re.compile(
-            "([0-9]+)(.+)"), i.get_text())
-        company_Name = re.findall(re.compile(
-            "([0-9]+)(.+)"), i.get_text())
-        company.append((company_Id, company_Name))
-    print(company)
+    for i in companyList_SZ:
+        i = i.get_text()
+        company_Id = re.search(re.compile(
+            "([0-9]+) (.+)"), i).group(1)
+        company_Name = re.search(re.compile(
+            "([0-9]+) (.+)"), i).group(2).strip()
+        company_Place = "深市主板"
+        company.append((company_Id, company_Name, company_Place))
+    for i in companyList_ZX:
+        i = i.get_text()
+        company_Id = re.search(re.compile(
+            "([0-9]+) (.+)"), i).group(1)
+        company_Name = re.search(re.compile(
+            "([0-9]+) (.+)"), i).group(2).strip()
+        company_Place = "中小企业板"
+        company.append((company_Id, company_Name, company_Place))
+    for i in companyList_CY:
+        i = i.get_text()
+        company_Id = re.search(re.compile(
+            "([0-9]+) (.+)"), i).group(1)
+        company_Name = re.search(re.compile(
+            "([0-9]+) (.+)"), i).group(2).strip()
+        company_Place = "创业板"
+        company.append((company_Id, company_Name, company_Place))
+    for i in companyList_SH:
+        i = i.get_text()
+        company_Id = re.search(re.compile(
+            "([0-9]+) (.+)"), i).group(1)
+        company_Name = re.search(re.compile(
+            "([0-9]+) (.+)"), i).group(2).strip()
+        company_Place = "沪市主板"
+        company.append((company_Id, company_Name, company_Place))
+    sql = "INSERT INTO company(company_Id,company_Name,company_Place) VALUES (%s,%s,%s) "
+    cur.executemany(sql, company)
+    conn.commit()
+    cur.close()
+    conn.close()
 
+
+def financial_collection(year, mm, family, company_Id):
+    url = 'http://www.cninfo.com.cn/information/stock/{0}_.jsp?stockCode={1}'.format(
+        family, company_Id)
+    # type可选参数包括：incomestatements，balancesheet，cashflow，financialreport，注意变更url和data中对应的参数;
+    # year格式：2015；mm格式：-12-31，-09-30，-06-30，-03-31
+    headers = {
+        'Host': 'www.cninfo.com.cn',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'Accept': 'text/css,*/*;q=0.1',
+        'Connection': 'keep-alive'
+    }
+    data = {'yyyy': year,
+            'mm': mm,
+            'cwzb': family
+            }
+    html = requests.post(url, headers=headers,
+                         allow_redirects=False, data=data)
+    print(html.encoding)
+    html.encoding = 'gbk'
+    print(html.text)
 
 if __name__ == "__main__":
     company_Collection()
+    financial_collection('2015', '-12-31', 'balancesheet', '000002')
